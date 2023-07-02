1bfd804b34b69c044bb7714ae1bd0d8b98ee970f
whitney-higgins <whitneyxhiggins@gmail.com>
2023-07-01 22:45:32 -0500

    data added to state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+// This component will hold state
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
